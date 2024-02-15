61a0236e2e8cc9d8834987a83523257d3fcb7a8a
A. Dhani41 <110752076+Ererere911@users.noreply.github.com>
2024-02-15 11:26:19 +0700

    .

--- database/seeders/PerusahaanSeeder.php
+++ database/seeders/PerusahaanSeeder.php
@@ -15,25 +15,65 @@ class PerusahaanSeeder extends Seeder
     {
         $users = [
             [
-                'id' => 1,
+                'id' => 8643846,
                 'perusahaan' => 'Pharos Group',
                 'level' => 'Pharos'
             ],[
-                'id' => 2,
+                'id' => 1434252,
                 'perusahaan' => 'PT. All Qurrny Bagas Pratama',
                 'level' => 'Qurrny'
             ],[
-                'id' => 3,
+                'id' => 3123463,
                 'perusahaan' => 'PT. Hana Anargya',
                 'level' => 'Hana'
             ],[
-                'id' => 4,
+                'id' => 2432364,
                 'perusahaan' => 'Cirill Indonesia',
                 'level' => 'Cirill'
             ],[
-                'id' => 5,
+                'id' => 5377296,
                 'perusahaan' => 'AnterAja',
                 'level' => 'Anteraja'
+            ],[
+                'id' => 8968634,
+                'perusahaan' => 'Trapo Indonesia',
+                'level' => 'Trapo'
+            ],[
+                'id' => 9864462,
+                'perusahaan' => 'Indomaret Group',
+                'level' => 'Indomaret'
+            ],[
+                'id' => 9565489,
+                'perusahaan' => 'Sigma Solusi Servis',
+                'level' => '3S'
+            ],[
+                'id' => 2432532,
+                'perusahaan' => 'Sutindo',
+                'level' => 'Sutindo'
+            ],[
+                'id' => 7654645,
+                'perusahaan' => 'Saham Rakyat',
+                'level' => 'Saham'
+            ],[
+                'id' => 3541477,
+                'perusahaan' => 'Asosiasi Penyelenggara Pemagangan Luar Negeri',
+                'level' => 'AP2LN'
+            ],[
+                'id' => 5786878,
+                'perusahaan' => 'Indivara Group',
+                'level' => 'Indivara'
+            ],[
+                'id' => 1131412,
+                'perusahaan' => 'PT. Prima Fajar Cahaya Surya',
+                'level' => 'Fajar'
+            ],[
+                'id' => 1353653,
+                'perusahaan' => 'Indivara Group',
+                'level' => 'Anteraja'
+            ],[
+                'id' => 1872744,
+                'perusahaan' => 'Infomedia',
+                'level' => 'Infomedia'
             ]
         ];
 
